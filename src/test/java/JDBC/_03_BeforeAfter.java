80846c7f73dd3778697836a5dc5b35da81b9b351
lilliia5 <lilliasavitska@gmail.com>
2023-11-06 20:39:28 -0500

    _03_class fixed

--- src/test/java/JDBC/_03_BeforeAfter.java
+++ src/test/java/JDBC/_03_BeforeAfter.java
@@ -4,35 +4,56 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class _03_BeforeAfter {
     Connection connection;
     Statement statement;
+
     @BeforeMethod
     public void DBConnection() throws SQLException {
         String url = "jdbc:mysql://db-technostudy.ckr1jisflxpv.us-east-1.rds.amazonaws.com:3306/sakila";
-        // url contains name of the technology we use to connect with database(jdbc), type of the database(mysql), address of the server
-        // of the database(//db-technostudy.ckr1jisflxpv.us-east-1.rds.amazonaws.com), port number(3306) and name of the database
-
         String username = "root";
         String password = "'\"-LhCB'.%k[4S]z";
-         connection = DriverManager.getConnection(url, username, password); // Creates a connection with the databse
 
-         statement = connection.createStatement();
+        connection = DriverManager.getConnection(url, username, password);
+
+        statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
     }
 
 
     @AfterMethod
-    public void DBConnection() throws SQLException {
+    public void DBConnectionClose() throws SQLException {
         connection.close();
     }
-    @Test 
-    public void test1(){
 
+
+    @Test
+    public void test1() throws SQLException {
+        ResultSet rs = statement.executeQuery("select * from language");
+
+        rs.next(); // We are on the first row
+        String language = rs.getString("name");
+        System.out.println("language = " + language);
+
+        rs.next(); // We are on the second row
+        String language1 = rs.getString("name");
+        System.out.println("language1 = " + language1);
     }
 
+    @Test
+    public void test2() throws SQLException {
+        ResultSet rs = statement.executeQuery("select * from language");
+        rs.next(); // We are on the first row
+        String language = rs.getString("name");
+        System.out.println("language = " + language);
+
+        rs.next(); // We are on the second row
+        String language1 = rs.getString("name");
+        System.out.println("language1 = " + language1);
+
+        rs.previous(); // moves one step backwards to the first row
+        String language2 = rs.getString("name");
+        System.out.println("language2 = " + language2);
+    }
 }
